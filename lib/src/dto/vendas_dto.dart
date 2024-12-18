124134a2c19fe3dd8a0491d340ee4f68817873d5
drelocatelli <andressalocatelli@hotmail.com>
2024-12-18 16:26:48 -0300

    gerenciar vendas

--- lib/src/dto/vendas_dto.dart
+++ lib/src/dto/vendas_dto.dart
@@ -6,7 +6,21 @@ class Venda {
   String responsavel;
   double valor;
   String data;
+  int numero;
 
-  Venda({required this.id, required this.nome, required this.produto, required this.categoria, required this.responsavel, required this.valor, required this.data});
+  Venda({required this.id, required this.nome, required this.produto, required this.categoria, required this.responsavel, required this.valor, required this.data, this.numero = 0});
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'nome': nome,
+      'produto': produto,
+      'categoria': categoria,
+      'responsavel': responsavel,
+      'valor': valor,
+      'data': data,
+      'numero': numero,
+    };
+  }
 }
 
--- lib/src/pages/dashboard/vendas/vendas_page.dart
+++ lib/src/pages/dashboard/vendas/vendas_page.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:racoon_tech_panel/src/dto/vendas_dto.dart';
 import 'package:racoon_tech_panel/src/layout/main_layout.dart';
@@ -22,10 +24,7 @@ class _VenddasState extends State<Vendas> {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Text('Gerenciar vendas', style: Theme.of(context).textTheme.headlineMedium),
-            SingleChildScrollView(
-              scrollDirection: Axis.horizontal,
-              child: _vendasTable(maxWidth)
-            ),
+            _vendasTable(maxWidth),
           ],
         )
       ),
@@ -47,62 +46,202 @@ Widget _vendasTable(double maxWidth) {
     Venda(id: 9, nome: 'Carlos', produto: 'Produto A', categoria: 'Categoria 3', responsavel: 'Beltrano', valor: 850.60, data: '04/30/2024'),
     Venda(id: 10, nome: 'Ana', produto: 'Produto B', categoria: 'Categoria 1', responsavel: 'Fulano', valor: 450.30, data: '09/20/2024'),
   ];
+
+  vendas.asMap().entries.toList()
+    ..sort((a, b) => a.key.compareTo(b.key))
+    ..forEach((entry) => entry.value.numero = entry.key + 1);
+
+  int _sortColumnIdx = 0; // coluna de data
+  bool _isAscending = true;
+  List<bool> selection = List<bool>.generate(vendas.length, (int index) => false);
   
   return StatefulBuilder(
     builder: (context, setState) {
-      return DataTable(
-        columns: [
-          DataColumn(label: Text('ID')),
-          DataColumn(label: Text('Nome')),
-          DataColumn(label: Text('Produto')),
-          DataColumn(label: Text('Categoria')),
-          DataColumn(label: Text('Responsável')),
-          DataColumn(label: Text('Valor')),
-          DataColumn(label: Text('Data')),
-          DataColumn(label: Text('Ações')),
-        ],
-        rows: vendas.map((venda) {
-          return DataRow(
-            cells: [
-              DataCell(Text(venda.id.toString())),
-              DataCell(Text(venda.nome)),
-              DataCell(Text(venda.produto)),
-              DataCell(Text(venda.categoria)),
-              DataCell(Text(venda.responsavel)),
-              DataCell(Text("R\$ ${venda.valor.toString()}")),
-              DataCell(Text(venda.data)),
-              DataCell(
-                Visibility(
-                  visible: maxWidth > 800,
-                  child: Row(
-                    children: [
-                      IconButton(
-                        icon: Icon(Icons.edit),
-                        onPressed: () {
-                          // Lógica para editar a venda
-                        },
-                      ),
-                      IconButton(
-                        icon: Icon(Icons.delete),
-                        onPressed: () {
-                          // Lógica para excluir a venda
-                        },
+      return Column(
+        children: [
+          Align(
+            alignment: Alignment.bottomRight, 
+            child: Visibility(
+              visible: selection.contains(true),
+              child: TextButton(
+                onPressed: () {
+                   List<int> selectedsIdxs = selection.asMap().entries
+                    .where((entry) => entry.value == true)
+                    .map((entry) => entry.key)
+                    .toList();
+              
+                  List filteredVendas = List.generate(vendas.length, (index) => 
+                    selectedsIdxs.contains(index) ? vendas[index] : null).where((item) => item != null).toList();
+                  
+                  List filteredProdutos = filteredVendas.map((item) => item.produto).toList();
+
+                  showDialog(
+                    context: context, 
+                    builder: (BuildContext context) {
+                      return AlertDialog(
+                        title: Text("Excluir selecionados"),
+                        content: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Text("Você tem certeza que deseja excluir as vendas selecionadas?"),
+                            Text("${filteredProdutos.join(', ')} serão deletados permanentemente."),
+                          ],
+                        ),
+                        actions: [
+                          TextButton(
+                            onPressed: () {
+                              Navigator.of(context).pop();
+                            }, 
+                            child: Text("Cancelar")
+                          ),
+                          TextButton(
+                            onPressed: () {
+                              setState(() {
+                                vendas.removeWhere((item) => filteredProdutos.contains(item.produto));
+                                selection = List<bool>.generate(vendas.length, (int index) => false);
+                              });
+                              Navigator.of(context).pop();
+                            }, 
+                            child: Text("Confirmar")
+                          ),
+                        ],
+                      );
+                    }
+                  );
+                }, 
+                child: Text("Excluir selecionados")
+              ),
+            )
+          ),
+          SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: SizedBox(
+              width: maxWidth >= 800 ? maxWidth : null,
+              child: DataTable(
+                sortColumnIndex: _sortColumnIdx,
+                sortAscending: _isAscending,
+                showCheckboxColumn: true,
+                columns: [
+                  DataColumn(
+                    label: Text('N°'),
+                    onSort: (columnIndex, ascending) => 
+                      setState(() {
+                        _sortColumnIdx = columnIndex;
+                        _isAscending = ascending;
+                        vendas.sort((a, b) => _isAscending ? a.numero.compareTo(b.numero) : b.numero.compareTo(a.numero));
+                      }),
+                  ),
+                  DataColumn(
+                    label: Text('Nome'),
+                    onSort: (columnIndex, ascending) => 
+                      setState(() {
+                        _sortColumnIdx = columnIndex;
+                        _isAscending = ascending;
+                        vendas.sort((a, b) => _isAscending ? a.nome.compareTo(b.nome) : b.nome.compareTo(a.nome));
+                      }),
+                  ),
+                  DataColumn(
+                    label: Text('Produto'),
+                    onSort: (columnIndex, ascending) => 
+                      setState(() {
+                        _sortColumnIdx = columnIndex;
+                        _isAscending = ascending;
+                        vendas.sort((a, b) => _isAscending ? a.produto.compareTo(b.produto) : b.produto.compareTo(a.produto));
+                      }),
+                  ),
+                  DataColumn(
+                    label: Text('Categoria'),
+                    onSort: (columnIndex, ascending) => 
+                      setState(() {
+                        _sortColumnIdx = columnIndex;
+                        _isAscending = ascending;
+                        vendas.sort((a, b) => _isAscending ? a.categoria.compareTo(b.categoria) : b.categoria.compareTo(a.categoria));
+                      }),
+                  ),
+                  DataColumn(
+                    label: Text('Responsável'),
+                    onSort: (columnIndex, ascending) => 
+                      setState(() {
+                        _sortColumnIdx = columnIndex;
+                        _isAscending = ascending;
+                        vendas.sort((a, b) => _isAscending ? a.responsavel.compareTo(b.responsavel) : b.responsavel.compareTo(a.responsavel));
+                      }),
+                  ),
+                  DataColumn(
+                    label: Text('Valor'),
+                    onSort: (columnIndex, ascending) => 
+                      setState(() {
+                        _sortColumnIdx = columnIndex;
+                        _isAscending = ascending;
+                        vendas.sort((a, b) => _isAscending ? a.valor.compareTo(b.valor) : b.valor.compareTo(a.valor));
+                      }),
+                  ),
+                  DataColumn(
+                    label: Text('Data'),
+                    onSort: (columnIndex, ascending) => 
+                      setState(() {
+                        _sortColumnIdx = columnIndex;
+                        _isAscending = ascending;
+                        vendas.sort((a, b) => _isAscending ? a.data.compareTo(b.data) : b.data.compareTo(a.data));
+                      }),
+                  ),
+                  DataColumn(
+                    label: Text('Ações'),
+                  ),
+                ],
+                rows: vendas.asMap().entries.map((entry) {
+                  final key = entry.key;
+                  final venda = entry.value;
+                  return DataRow(
+                    selected: selection[entry.key],
+                    onSelectChanged: (value) {
+                      setState(() {
+                        selection[key] = value!;
+                      });
+                    },
+                    cells: [
+                      DataCell(Text(venda.numero.toString())),
+                      DataCell(Text(venda.nome)),
+                      DataCell(Text(venda.produto)),
+                      DataCell(Text(venda.categoria)),
+                      DataCell(Text(venda.responsavel)),
+                      DataCell(Text("R\$ ${venda.valor.toString()}")),
+                      DataCell(Text(venda.data)),
+                      DataCell(
+                        Visibility(
+                          visible: maxWidth > 800,
+                          child: Row(
+                            children: [
+                              IconButton(
+                                icon: Icon(Icons.edit),
+                                onPressed: () {
+                                  // Lógica para editar a venda
+                                },
+                              ),
+                              IconButton(
+                                icon: Icon(Icons.delete),
+                                onPressed: () {
+                                  // Lógica para excluir a venda
+                                },
+                              ),
+                            ],
+                          ),
+                          replacement: PopupMenuButton(
+                            icon: Icon(Icons.more_vert),
+                            itemBuilder: (context) {
+                              final popupItems = _editAndDeleteIco(venda, maxWidth).map((item) => PopupMenuItem(child: Center(child: item,), onTap: () { item.onPressed!(); })).toList();
+                                return popupItems;
+                            },
+                          ),
+                        ),
                       ),
                     ],
-                  ),
-                  replacement: PopupMenuButton(
-                    icon: Icon(Icons.more_vert),
-                    itemBuilder: (context) {
-                      final popupItems = _editAndDeleteIco(venda, maxWidth).map((item) => PopupMenuItem(child: Center(child: item,), onTap: () { item.onPressed!(); })).toList();
-
-                        return popupItems;
-                    },
-                  ),
-                ),
+                  );
+                }).toList(),
               ),
-            ],
-          );
-        }).toList(),
+            ),
+          ),
+        ],
       );
     }
   );
@@ -126,7 +265,6 @@ List<IconButton> _editAndDeleteIco(item, maxWidth) {
       },
     ),
   ];
-
 
   return items;
 }
